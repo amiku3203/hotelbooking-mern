ced6cf49a2233146d97bbbc2689fdef0b5df86de
amit <kumaramit28538@gmail.com>
2024-03-10 18:01:42 +0530

    fix err

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,11 +35,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
-app.use(cors({
-    origin: "*",
-    credentials :true
-
-} ));
+ app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 
 app.use(express.static(path.join(__dirname,"../../frontend/dist")))
