acd95aaa98e307d87e57475eb95c68699fd4511d
amit <kumaramit28538@gmail.com>
2024-03-10 16:35:08 +0530

    fix err

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,52 +1,57 @@
-import { useForm } from 'react-hook-form';
-import { useMutation, useQueryClient } from 'react-query';
-import * as apiClient from "../api-client"
-import { useAppContext } from '../contexts/AppContexts';
-import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContexts";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
 export type SignInFormData = {
   email: string;
   password: string;
 };
 
 const SignIn = () => {
-  const {showToast}= useAppContext();
+  const { showToast } = useAppContext();
   const navigate = useNavigate();
-  const { register, formState: { errors },handleSubmit } = useForm<SignInFormData>();
-  const queryClient=useQueryClient();
+  const queryClient = useQueryClient();
 
- const mutation= useMutation(apiClient.signIn,{
-   onSuccess: async()=>{
-    //show the toast
-     showToast({message:"Sign Successful" , type:"SUCCESS"})
-    // navigate to the home page 
-    await queryClient.invalidateQueries("validateToken");
-     navigate("/");
-   },
-   onError: async(error:Error)=>{
-      //show the toast
-      console.log(error)
-      showToast({message:error.message, type:"ERROR"})
-   }
- });
- 
- const onSubmit= handleSubmit((data)=>{
-   mutation.mutate(data);
- })
+  const location = useLocation();
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<SignInFormData>();
+
+  const mutation = useMutation(apiClient.signIn, {
+    onSuccess: async () => {
+      showToast({ message: "Sign in Successful!", type: "SUCCESS" });
+      await queryClient.invalidateQueries("validateToken");
+      navigate(location.state?.from?.pathname || "/");
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
   return (
-    <form className='flex flex-col gap-5' onSubmit={onSubmit}>
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
       <h2 className="text-3xl font-bold">Sign In</h2>
-      <label className="text-gray-700 text-sm font-bold">
+      <label className="text-gray-700 text-sm font-bold flex-1">
         Email
         <input
           type="email"
           className="border rounded w-full py-1 px-2 font-normal"
           {...register("email", { required: "This field is required" })}
-        />
+        ></input>
         {errors.email && (
           <span className="text-red-500">{errors.email.message}</span>
         )}
       </label>
-      <label className="text-gray-700 text-sm font-bold">
+      <label className="text-gray-700 text-sm font-bold flex-1">
         Password
         <input
           type="password"
@@ -58,22 +63,25 @@ const SignIn = () => {
               message: "Password must be at least 6 characters",
             },
           })}
-        />
+        ></input>
         {errors.password && (
           <span className="text-red-500">{errors.password.message}</span>
         )}
       </label>
-      <span className='flex items-center justify-between'>
-         <span className='text-sm'>
-           Not Registered? <Link to="/register" className='underline'>Create an account here  </Link>
-         </span>
+      <span className="flex items-center justify-between">
+        <span className="text-sm">
+          Not Registered?{" "}
+          <Link className="underline" to="/register">
+            Create an account here
+          </Link>
+        </span>
         <button
           type="submit"
           className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
         >
           Login
         </button>
-        </span>
+      </span>
     </form>
   );
 };
