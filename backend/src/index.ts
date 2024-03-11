25a23522500ca300743cbbaa6ce712213dc5cfb5
amiku3203 <121296949+amiku3203@users.noreply.github.com>
2024-03-12 03:20:01 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin:"*",
+    origin:"https://sleepscapepro.netlify.app",
     credentials: true,
   })
 );
