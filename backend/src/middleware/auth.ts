81698a01822ad34e8d411c5ffd24399ecd405329
amiku3203 <121296949+amiku3203@users.noreply.github.com>
2024-03-11 17:54:23 +0530

    Update auth.ts

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -12,17 +12,17 @@ declare global {
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies["auth_token"];
   console.log("token",token)
-  if (!token) {
-    return res.status(401).json({ message: "unauthorized" });
-  }
+  // if (!token) {
+  //   return res.status(401).json({ message: "unauthorized" });
+  // }
 
-  try {
+  // try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-    req.userId = (decoded as JwtPayload).userId;
+  //   req.userId = (decoded as JwtPayload).userId;
     next();
-  } catch (error) {
-    return res.status(401).json({ message: "unauthorized" });
-  }
+  // } catch (error) {
+  //   return res.status(401).json({ message: "unauthorized" });
+  // }
 };
 
 export default verifyToken;
