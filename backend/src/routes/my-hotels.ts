c24798d6f67deff64d9ee62b6429d409386c6bc4
amiku3203 <121296949+amiku3203@users.noreply.github.com>
2024-03-13 05:03:38 +0530

    Update my-hotels.ts

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -18,6 +18,7 @@ const upload = multer({
 
 router.post(
   "/",
+  verifyToken,
   [
     body("name").notEmpty().withMessage("Name is required"),
     body("city").notEmpty().withMessage("City is required"),
