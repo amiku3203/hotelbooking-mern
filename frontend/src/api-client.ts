aa6e48dd5abc9df6335bbee71f7382dfb057d968
amiku3203 <121296949+amiku3203@users.noreply.github.com>
2024-03-14 01:23:28 +0530

    Update api-client.ts

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -37,12 +37,13 @@ export const register = async (formData: RegisterFormData) => {
 };
 
  export const signIn = async (formData: SignInFormData) => {
+  const token = localStorage.getItem("token");
   const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
     method: "POST",
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
-      'Authorization': localStorage.getItem("token")
+      'Authorization': `Bearer ${token}`
     },
     body: JSON.stringify(formData),
   });
